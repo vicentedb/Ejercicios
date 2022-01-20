3823d45795bd7c0c5d96c3a46185fde0e6673b1a
vicentedb <63682432+vicentedb@users.noreply.github.com>
2022-01-20 10:31:26 +0100

    Create ejercicio3-edad.py

---
+++ ejercicio3-edad.py
@@ -0,0 +1,7 @@
+'''Crear un programa que pida la edad y te indique si eres mayor de edad o no.'''
+
+edad = input(str("Dame tu edad: "))
+if edad >= '18':
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
