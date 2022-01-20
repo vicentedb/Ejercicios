8325e6f5c5e26f7c03607a0548bb47ae6ed342e3
vicentedb <63682432+vicentedb@users.noreply.github.com>
2022-01-20 10:51:43 +0100

    Create ejercicio4-saludador-evaluador-edad.py

---
+++ ejercicio4-saludador-evaluador-edad.py
@@ -0,0 +1,7 @@
+print("Hola!")
+nombre=input(str("\nDime tu nombre: "))
+edad=input(str("\nDame tu edad: "))
+if edad > '17':
+    print("\nEres mayor de edad.")
+else:
+    print("\nMenor de edad.")
