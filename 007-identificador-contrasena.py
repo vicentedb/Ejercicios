a34e404b794f7dc55c83ef280a5f1dd433f194ba
vicentedb <63682432+vicentedb@users.noreply.github.com>
2022-01-21 11:53:44 +0100

    333

---
+++ 007-identificador-contrasena.py
@@ -0,0 +1,15 @@
+#Pedir al usuario un identificador y una contraseña
+#La contraseña no puede tener las palabras:
+#ADMIN
+#123
+#PASSWORD
+
+'''print ("Hola!")
+usuario = input("Introduce el usuario: \n")
+contrasena = input("Introduce la contraseña: \n")
+if contrasena.find('ADMIN','123','PASSWORD'):
+    print("Error")
+else:
+    print("Adelante")'''
+
+    
